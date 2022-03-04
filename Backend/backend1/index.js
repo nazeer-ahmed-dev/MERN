b82869d616c6c17c2a9ea6e3bd28b557a07a4d99
unknown <nazeerahmedofficial1.com>
2022-03-04 11:19:49 +0500

    connected with moongodb

--- Backend/backend1/index.js
+++ Backend/backend1/index.js
@@ -7,7 +7,7 @@ const FoodModel = require("./model/Food")
 // front end result will be return in json format
 app.use(express.json())
 
-
+// this string we can use in .env file to secure i have used here for practise purpose
 mongoos.connect("mongodb+srv://nazeer_ahemd_25:Tharparkar2513@crud.klgms.mongodb.net/Crud_food?retryWrites=true&w=majority",{
     useNewUrlParser: true,
 })
